5fa17083410384dcf69e3efbfc523916e361b2ca
AmadeuszLakoma <amadeuszlakoma@poczta.fm>
2017-06-22 06:17:43 +0200

    TM
    Steganografia + Catalana

---
+++ SteganografiaTM C#/SteganografiaTM/OKNO.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SteganografiaTM
+{
+    partial class OKNO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Podglad = new System.Windows.Forms.PictureBox();
+            this.OtworzPlik = new System.Windows.Forms.Button();
+            this.Ukryj = new System.Windows.Forms.Button();
+            this.Odkryj = new System.Windows.Forms.Button();
+            this.PoleSciezki = new System.Windows.Forms.TextBox();
+            this.PoleWiadomosci = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Generuj = new System.Windows.Forms.Button();
+            this.PodajN = new System.Windows.Forms.TextBox();
+            this.WprowadzKod = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Podglad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Podglad
+            // 
+            this.Podglad.Location = new System.Drawing.Point(12, 12);
+            this.Podglad.Name = "Podglad";
+            this.Podglad.Size = new System.Drawing.Size(431, 223);
+            this.Podglad.TabIndex = 0;
+            this.Podglad.TabStop = false;
+            // 
+            // OtworzPlik
+            // 
+            this.OtworzPlik.Location = new System.Drawing.Point(107, 253);
+            this.OtworzPlik.Name = "OtworzPlik";
+            this.OtworzPlik.Size = new System.Drawing.Size(75, 23);
+            this.OtworzPlik.TabIndex = 1;
+            this.OtworzPlik.Text = "Otwórz";
+            this.OtworzPlik.UseVisualStyleBackColor = true;
+            this.OtworzPlik.Click += new System.EventHandler(this.OtworzPlikKlik);
+            // 
+            // Ukryj
+            // 
+            this.Ukryj.Location = new System.Drawing.Point(107, 324);
+            this.Ukryj.Name = "Ukryj";
+            this.Ukryj.Size = new System.Drawing.Size(75, 23);
+            this.Ukryj.TabIndex = 2;
+            this.Ukryj.Text = "Ukryj";
+            this.Ukryj.UseVisualStyleBackColor = true;
+            this.Ukryj.Click += new System.EventHandler(this.UkryjKlik);
+            // 
+            // Odkryj
+            // 
+            this.Odkryj.Location = new System.Drawing.Point(188, 324);
+            this.Odkryj.Name = "Odkryj";
+            this.Odkryj.Size = new System.Drawing.Size(75, 23);
+            this.Odkryj.TabIndex = 3;
+            this.Odkryj.Text = "Odkryj";
+            this.Odkryj.UseVisualStyleBackColor = true;
+            this.Odkryj.Click += new System.EventHandler(this.OdkryjKlik);
+            // 
+            // PoleSciezki
+            // 
+            this.PoleSciezki.Location = new System.Drawing.Point(206, 256);
+            this.PoleSciezki.Name = "PoleSciezki";
+            this.PoleSciezki.Size = new System.Drawing.Size(141, 20);
+            this.PoleSciezki.TabIndex = 4;
+            // 
+            // PoleWiadomosci
+            // 
+            this.PoleWiadomosci.Location = new System.Drawing.Point(206, 291);
+            this.PoleWiadomosci.Name = "PoleWiadomosci";
+            this.PoleWiadomosci.Size = new System.Drawing.Size(141, 20);
+            this.PoleWiadomosci.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Dane tekstowe";
+            // 
+            // Generuj
+            // 
+            this.Generuj.Location = new System.Drawing.Point(368, 365);
+            this.Generuj.Name = "Generuj";
+            this.Generuj.Size = new System.Drawing.Size(75, 23);
+            this.Generuj.TabIndex = 7;
+            this.Generuj.Text = "Generuj";
+            this.Generuj.UseVisualStyleBackColor = true;
+            this.Generuj.Click += new System.EventHandler(this.Generuj_Click);
+            // 
+            // PodajN
+            // 
+            this.PodajN.Location = new System.Drawing.Point(368, 339);
+            this.PodajN.Name = "PodajN";
+            this.PodajN.Size = new System.Drawing.Size(75, 20);
+            this.PodajN.TabIndex = 8;
+            // 
+            // WprowadzKod
+            // 
+            this.WprowadzKod.Location = new System.Drawing.Point(287, 365);
+            this.WprowadzKod.Name = "WprowadzKod";
+            this.WprowadzKod.Size = new System.Drawing.Size(75, 23);
+            this.WprowadzKod.TabIndex = 9;
+            this.WprowadzKod.Text = "Kod";
+            this.WprowadzKod.UseVisualStyleBackColor = true;
+            this.WprowadzKod.Click += new System.EventHandler(this.WprowadzKod_Click);
+            // 
+            // OKNO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 400);
+            this.Controls.Add(this.WprowadzKod);
+            this.Controls.Add(this.PodajN);
+            this.Controls.Add(this.Generuj);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PoleWiadomosci);
+            this.Controls.Add(this.PoleSciezki);
+            this.Controls.Add(this.Odkryj);
+            this.Controls.Add(this.Ukryj);
+            this.Controls.Add(this.OtworzPlik);
+            this.Controls.Add(this.Podglad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "OKNO";
+            this.Text = "SteganografiaTM";
+            ((System.ComponentModel.ISupportInitialize)(this.Podglad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Podglad;
+        private System.Windows.Forms.Button OtworzPlik;
+        private System.Windows.Forms.Button Ukryj;
+        private System.Windows.Forms.Button Odkryj;
+        private System.Windows.Forms.TextBox PoleSciezki;
+        private System.Windows.Forms.TextBox PoleWiadomosci;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Generuj;
+        private System.Windows.Forms.TextBox PodajN;
+        private System.Windows.Forms.Button WprowadzKod;
+    }
+}
+
---
+++ SteganografiaTM C#/SteganografiaTM/OKNO.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SteganografiaTM
+{
+    public partial class OKNO : Form
+    {
+        int w,A;
+        string kod;
+        public OKNO()
+        {
+            InitializeComponent();
+        }
+
+        private void OtworzPlikKlik(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image Files (*.bmp, *.png, *.jpg) | *.bmp; *.png; *.jpg";
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                PoleSciezki.Text = openDialog.FileName.ToString();
+                Podglad.ImageLocation = PoleSciezki.Text;
+            }
+        }
+
+
+        int POTENGAN(int n) //liczenie potęgi
+        { int  i;
+            int pot;
+            A = 2 * n;// zwracanie długości ciągów bitowych
+            w = 1;
+            pot = 2 * n;
+            for (i = 1; i <= pot; i++)
+            {
+                w = w * 2;   
+            }
+            return w;
+        }
+
+        void BIN()//ciągi binarne i zapis do pliku
+        {
+            String s;
+            int ls, ls1;
+            {
+              int  i = 0;
+              
+               
+                FileStream PLIKBIN = new FileStream(@"C:\RM\f.txt", FileMode.Create);
+               
+                for (i = 1; i <= w; i++)
+                {
+               
+                    s = Convert.ToString(i, 2);
+                    s = s.PadLeft(A, '0');//uzupełnianie "0" z lewej stroy
+                    int length = s.Length;
+                    int[] tablica = s.ToCharArray().Select(q => int.Parse(q.ToString())).ToArray();
+
+                    ls = 0;
+                    ls1 = 0;
+                    for (int ll = 1; ll <= length; ll++)
+
+                    {
+
+                        Console.WriteLine("s = " + tablica[ll-1]);
+                        if (s[ll-1] == '0') ls++; // LICZENIE ZER
+                        if (s[ll-1] == '1') ls1++;
+                        if (ls >= ls1)//sprawdzanie czy '0' jest co najmniej tyle amo co '1' - optymalizacja !!
+                        {
+                         //   Console.WriteLine("ls = " + ls + " ls1 = " + ls1);
+                          //  Console.WriteLine("ll = " + ll + " len = " + length);
+                            if (ll == length)//ciągi o zadanej długości
+                                if (ls == ls1)//tyle samo '0' i '1'
+                                {
+                                    byte[] bdata = Encoding.Default.GetBytes(s + Environment.NewLine);
+                                    PLIKBIN.Write(bdata, 0, bdata.Length);
+                                }
+                        }
+                        else break;
+                    }
+                }
+                PLIKBIN.Close();
+
+            }
+        }
+
+        private void Generuj_Click(object sender, EventArgs e)
+        {
+           int n = Int32.Parse(PodajN.Text);
+            if (n >= 0 && n <= 10)
+            {
+                POTENGAN(n);
+                BIN();
+                int licznik = 0;
+
+                foreach (string linie in File.ReadLines(@"C:\RM\f.txt"))//ile lini
+                {
+                    if (linie != String.Empty) ++licznik;
+                }
+                MessageBox.Show("Wygenerowano! " + licznik);
+            }
+            else MessageBox.Show("Nie wygenerowano! ");
+            
+        }
+
+
+        private void UkryjKlik(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(PoleSciezki.Text);
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < PoleWiadomosci.TextLength)
+                    {
+              
+                        
+                        char litera = Convert.ToChar(PoleWiadomosci.Text.Substring(j, 1));
+                        int wartosc = Convert.ToInt32(litera);
+                        int kodint = Convert.ToInt32(kod);
+                        // Console.WriteLine();
+                        
+                       // for (;(wartosc+kodint)>255;)
+                        //{ kodint = kodint / 10; }
+                        MessageBox.Show("wartosc " + wartosc + " kod " + kodint);
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, wartosc+kodint));
+                    }
+
+                    if (i == img.Width - 1 && j == img.Height - 1)
+                    {
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, PoleWiadomosci.TextLength));
+                    }
+                }
+            }
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+           
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                PoleSciezki.Text = saveFile.FileName.ToString();
+                Podglad.ImageLocation = PoleSciezki.Text;
+
+                img.Save(PoleSciezki.Text);
+            }
+        }
+
+        private void WprowadzKod_Click(object sender, EventArgs e)
+        {
+            int n = Int32.Parse(PodajN.Text);
+            if (n >= 0 && n <= 100)
+            {
+
+                int licznik = 0;
+
+                foreach (string linie in File.ReadLines(@"C:\RM\f.txt"))
+                {
+                    if (linie != String.Empty) ++licznik;
+                    if (licznik == n) {
+                        MessageBox.Show("Linia " + linie);
+                        kod = linie;
+                    }
+                }
+                
+                ///////////////////////////////////////////////
+            }
+            else MessageBox.Show("Nie wygenerowano! ");
+        }
+
+        private void OdkryjKlik(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(PoleSciezki.Text);
+            string wiadomosc = "";
+
+            Color lastpixel = img.GetPixel(img.Width - 1, img.Height - 1);
+            int DlugoscWiadomosci = lastpixel.B;
+            int kodint = Convert.ToInt32(kod);
+
+                for (int i = 0; i < img.Width; i++)
+                {
+                    for (int j = 0; j < img.Height; j++)
+                    {
+                        Color pixel = img.GetPixel(i, j);
+
+                        if (i < 1 && j < DlugoscWiadomosci)
+                        {
+                            int wartosc = pixel.B;
+                     //   for (; (wartosc + kodint) > 255;)
+                    //    { kodint = kodint / 10; }
+                        if (wartosc - kodint >= 0)
+                            {
+                                char c = Convert.ToChar(wartosc-kodint);
+                                MessageBox.Show("Wartosc " + wartosc + " kodint " + kodint + " c " + c);
+                                string litera = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
+                                wiadomosc = wiadomosc + litera;
+                            }
+
+                        }
+                    }
+                }
+          
+            PoleWiadomosci.Text = wiadomosc;
+        }
+    }
+}
